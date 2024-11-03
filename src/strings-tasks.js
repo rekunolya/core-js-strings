a10441e0671dc83544102b69a94c35061b91729c
rekunolya <rekun.olya@gmail.com>
2024-11-03 22:20:16 +0300

    add a solution to task countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -317,8 +317,15 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = 'aeiouyAEIOUY';
+  let count = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    if (vowels.indexOf(str[i]) !== -1) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
